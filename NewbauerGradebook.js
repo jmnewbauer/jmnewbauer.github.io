1c2ae96d1e6d5ece913b8a935fc34332b6fb43e1
jmnewbauer <jmnewbauer@my.waketech.edu>
2025-04-09 12:02:13 -0400

    Submitted HTML files and added a node

--- NewbauerGradebook.js
+++
@@ -1,8 +0,0 @@
-function fetchGradeData() {
-    console.log("fetching grade data...")
-}
-function populateGradebook(data) {
-    console.log("populating gradebook with data", data);
-}
-const gradeData = fetchGradeData();
-populateGradebook(gradeData)
