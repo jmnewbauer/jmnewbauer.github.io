a5ce1f220ead1ff3cfac82cc050198c5d9d90887
jmnewbauer <jmnewbauer@my.waketech.edu>
2025-04-22 17:44:53 -0400

    Add files via upload

--- public/NewbauerGradebook.js
+++ public/NewbauerGradebook.js
@@ -1,8 +1,34 @@
 function fetchGradeData() {
-    console.log("fetching grade data...")
+    console.log("fetching grade data...");
+    let xhr = new XMLHttpRequest();
+    let apiRoute = "/api/grades";
+    xhr.onreadystatechange = function(){
+        let results;
+        if(xhr.readyState === xhr.DONE){
+            if(xhr.status !==200){
+                console.error(`could not get grades.
+                    status: ${xhr.status}`);
+                }
+                populateGradebook(JSON.parse(xhr.responseText));
+            }
+        }.bind(this);
+        xhr.open("get", apiRoute, true);
+        xhr.send();
 }
 function populateGradebook(data) {
     console.log("populating gradebook with data", data);
+    let tableElm = document.getElementById("gradebook");
+    console.log(tableElm);
+    data.forEach(function(assignment){
+        let row = document.createElement("tr");
+        let columns = [];
+        columns.name = document.createElement('td');
+        columns.name.appendChild(
+            document.createTextNode(assignment.last_name + "," + assignment.first_name)
+        );
+        row.appendChild(columns.name);
+        row.appendChild(columns.grade);
+        tableElm.appendChild(row);
+});
 }
-const gradeData = fetchGradeData();
-populateGradebook(gradeData)
+    
